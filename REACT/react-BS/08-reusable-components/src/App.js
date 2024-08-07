d42a9fb7096bc1d9a7030cbaeec589146db19b3b
sergeistetsko <sergeistetsko2021@yandex.by>
2024-08-07 09:59:59 +0300

    add new project

---
+++ REACT/react-BS/08-reusable-components/src/App.js
@@ -0,0 +1,17 @@
+import './App.css'
+import MyComponent from './components/MyComponent.js'
+import OtherComponent from './components/OtherComponent.js'
+
+function App() {
+  return (
+  <div className="App">
+    <MyComponent />
+    <OtherComponent />
+    <MyComponent />
+    <OtherComponent />
+    <MyComponent />
+  </div>
+  )
+}
+
+export default App
---
+++ REACT/react-BS/08-reusable-components/src/components/OtherComponent.js
@@ -0,0 +1,9 @@
+function OtherComponent() {
+    return (
+      <div>
+        <p>Hello from the reusable component</p>
+      </div>
+    )
+  }
+
+export default OtherComponent
