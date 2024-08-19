025f5c387e378312464a29af153e6c295ff063d5
sergeistetsko <sergeistetsko2021@yandex.by>
2024-08-19 07:42:24 +0300

    add new project

---
+++ REACT/react-BS/11-state/src/App.js
@@ -0,0 +1,12 @@
+import './App.css'
+import RandomNumber from './components/RandomNumber.js'
+
+function App() {
+  return (
+    <div className="App">
+      <RandomNumber maxNum={1001}/>
+    </div>
+  )
+}
+
+export default App
