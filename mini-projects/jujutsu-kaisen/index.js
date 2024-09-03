f24d4b8ffdcfc5b08725e365470e85a47013e252
sergeistetsko <sergeistetsko2021@yandex.by>
2024-09-03 08:26:22 +0300

    add new changes

--- mini-projects/jujutsu-kaisen/index.js
+++ mini-projects/jujutsu-kaisen/index.js
@@ -4,6 +4,8 @@ const slider = {
   showPreviousButton: document.getElementById('show-previous-btn'),
   showNextButton: document.getElementById('show-next-btn'),
   slideImage: document.getElementById('slide-img'),
+  startButton: document.getElementById('first-pic'),
+  confirmPage: document.getElementById('confirm-page'),
   start: function () {
     const that = this
     this.showPreviousButton.addEventListener('click', function (e) {
@@ -12,10 +14,40 @@ const slider = {
     this.showNextButton.addEventListener('click', function (e) {
       that.onShowNextButtonClick(e)
     })
-    this.imagesUrls.push('img/part1/Том 1 Магическая битва_page-0001.jpg')
-    this.imagesUrls.push('img/part1/Том 1 Магическая битва_page-0002.jpg')
-    this.imagesUrls.push('img/part1/Том 1 Магическая битва_page-0003.jpg')
-    this.imagesUrls.push('img/part1/Том 1 Магическая битва_page-0004.jpg')
+    this.startButton.addEventListener('click', function (e) {
+      that.onStartButton(e)
+    })
+    this.confirmPage.addEventListener('click', function (e) {
+      that.onConfirmPage(e)
+    })
+    this.imagesUrls.push('img/part1/0001.jpg')
+    this.imagesUrls.push('img/part1/0002.jpg')
+    this.imagesUrls.push('img/part1/0003.jpg')
+    this.imagesUrls.push('img/part1/0004.jpg')
+    this.imagesUrls.push('img/part1/0005.jpg')
+    this.imagesUrls.push('img/part1/0006.jpg')
+    this.imagesUrls.push('img/part1/0007.jpg')
+    this.imagesUrls.push('img/part1/0008.jpg')
+    this.imagesUrls.push('img/part1/0009.jpg')
+    this.imagesUrls.push('img/part1/0010.jpg')
+    this.imagesUrls.push('img/part1/0011.jpg')
+    this.imagesUrls.push('img/part1/0012.jpg')
+    this.imagesUrls.push('img/part1/0013.jpg')
+    this.imagesUrls.push('img/part1/0014.jpg')
+    this.imagesUrls.push('img/part1/0015.jpg')
+    this.imagesUrls.push('img/part1/0016.jpg')
+    this.imagesUrls.push('img/part1/0017.jpg')
+    this.imagesUrls.push('img/part1/0018.jpg')
+    this.imagesUrls.push('img/part1/0019.jpg')
+    this.imagesUrls.push('img/part1/0020.jpg')
+    this.imagesUrls.push('img/part1/0021.jpg')
+    this.imagesUrls.push('img/part1/0022.jpg')
+    this.imagesUrls.push('img/part1/0023.jpg')
+    this.imagesUrls.push('img/part1/0024.jpg')
+    this.imagesUrls.push('img/part1/0025.jpg')
+    this.imagesUrls.push('img/part1/0026.jpg')
+    this.imagesUrls.push('img/part1/0027.jpg')
+    this.imagesUrls.push('img/part1/0028.jpg')
 
     this.slideImage.src = this.imagesUrls[this.currentImageIndex]
     this.showPreviousButton.disabled = true
@@ -27,6 +59,20 @@ const slider = {
   onShowNextButtonClick: function () {
     this.currentImageIndex++
     this.updateImage()
+  },
+  onStartButton: function () {
+    this.currentImageIndex = 0
+    this.updateImage()
+  },
+  onConfirmPage: function () {
+    const input = document.querySelector('.input-number-page')
+    const pageIndex = parseInt(input.value, 10) - 1 // Преобразуем значение input в индекс массива
+    if (pageIndex >= 0 && pageIndex < this.imagesUrls.length) {
+      this.currentImageIndex = pageIndex
+      this.updateImage()
+    } else {
+      alert('Неверный номер страницы')
+    }
   },
   updateImage: function () {
     this.slideImage.style.opacity = 0
